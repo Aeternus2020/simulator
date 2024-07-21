54c8dfca774ff5e33da6bdb2f09d4503eb70913e
Olena <crimeaodessa2006@gmail.com>
2024-07-21 21:25:00 +0200

    error with deploy

--- src/components/airplane/StyleAirplane.tsx
+++ src/components/airplane/StyleAirplane.tsx
@@ -3,11 +3,11 @@ import { Canvas } from '@react-three/fiber';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
-export const StyledAirplaneCanvas = styled(Canvas)(({ theme }) => ({
+export const StyledAirplaneCanvas = styled(Canvas)({
     // backgroundColor: theme.colors.primary,
     width: '300px!important',
     height: '260px!important',
-}));
+});
 
 export const StyledMotionDiv = styled(motion.div)(({ theme }) => ({
     // backgroundColor: 'red',
